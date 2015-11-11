bfdeecc6e809d6b81c863643b33300f64a72f43e
Shawn <schlosser.shawn@gmail.com>
2015-11-11 12:57:09 -0600

    Added picture gallery option modals

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,7 +1,7 @@
 (function(){
-    var app = angular.module('application', []);
+    var app = angular.module('application', ['ui.bootstrap', 'bootstrapLightbox']);
 
-    app.controller('mainController', ['$scope', '$interval', function($scope, $interval) {
+    app.controller('mainController', ['$scope', '$interval', 'Lightbox', function($scope, $interval, Lightbox) {
         $scope.image1 = 'assets/images/blueLake.JPG';
         var bgColors = ['lightcoral','lightgoldenrodyellow','lightgreen', 'lightgrey', 'lightpink', 'lightsalmon',
         'lightseagreen', 'lightskyblue'];
@@ -19,6 +19,22 @@
                 $scope.colorCount = 0;
             }
         }, 10000);
+
+        $scope.images = [
+            {
+                'url': 'app/assets/images/mortgageHome.png',
+                'thumbUrl': 'app/assets/images/angular.png'
+            },
+            {
+                'url': 'app/assets/images/mortgage-calculator.png',
+                'thumbUrl': 'app/assets/images/angular.png',
+                'caption': 'hey!'
+            }
+        ];
+
+        $scope.openLightboxModal = function (index) {
+            Lightbox.openModal($scope.images, index);
+        };
 
     }]);
 })();
